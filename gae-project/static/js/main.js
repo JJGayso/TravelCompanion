a0f519e7d5d9fc9f70223846fefd3867fa3724f1
Trevor Burch <burchtm@rose-hulman.edu>
2016-10-17 17:17:27 -0400

    Modified things for the start of getting a basic layout together.

--- gae-project/static/js/main.js
+++ gae-project/static/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    console.log("Hello, JavaScript world!");
+
 });
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.465542, lng: -87.375924},
+        zoom: 11
+    });
+    google.maps.event.trigger(map, "resize");
+}
