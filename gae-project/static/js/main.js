8aa852a53eefd2308b40a8b63dfc3674732a8dcd
Trevor Burch <burchtm@rose-hulman.edu>
2016-10-26 18:05:47 -0400

    Route Modal Now Shows

--- gae-project/static/js/main.js
+++ gae-project/static/js/main.js
@@ -1,6 +1,45 @@
 $(document).ready(function() {
     $(window).off("resize");
+    mdlInitializations();
+    enableButtons();
 });
+
+enableButtons = function() {
+    $(".edit-route-btn")
+        .click(
+            function() {
+                document.querySelector('#edit-route-dialog').showModal();
+                var stop1 = $(this).find(".stop1").html();
+                var stop2 = $(this).find(".stop2").html();
+                var stop3 = $(this).find(".stop3").html();
+
+                // Note that I had to use change the mdl way to get the input label to float up.
+                // See: https://github.com/google/material-design-lite/issues/1287
+                document.querySelector('#stop1-field').MaterialTextfield
+                    .change(stop1);
+                document.querySelector('#stop2-field').MaterialTextfield
+                    .change(stop2);
+                document.querySelector('#stop3-field').MaterialTextfield
+                    .change(stop3);
+            });
+
+    // Password cancel button to close the insert-password-dialog
+    $('.close-edit-route-dialog').click(function() {
+        document.querySelector('#edit-route-dialog').close();
+    });
+}
+
+mdlInitializations = function() {
+    // Polyfill for browsers that don't support the dialog tag.
+    var dialogs = document.querySelectorAll('dialog');
+    for (var i = 0; i < dialogs.length; i++) {
+        // Using an old school style for loop since this for compatibility.
+        var dialog = dialogs[i];
+        if (!dialog.showModal) {
+            dialogPolyfill.registerDialog(dialog);
+        }
+    }
+};
 
 var map;
 function initMap() {
